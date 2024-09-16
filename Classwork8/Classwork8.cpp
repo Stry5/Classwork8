f4ec95a9c5d3010222c181060a48054c8193feb9
Glenn <glenndavidmah@gmail.com>
2024-09-15 19:34:49 -0700

    adding files

---
+++ Classwork8/Classwork8.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        int x = 10.0;
+
+        int y = 3.0;
+
+        double z = x / y;
+
+        cout << z;
+
+        return 0;
+
+}
+
